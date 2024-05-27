1bafd93c55416a81eaf0f9bb8bd7002ea3a7fb7b
Emma Åstrand <aastrand.emma@gmail.com>
2024-05-27 20:22:23 +0200

    Initial commit

---
+++ src/main/java/org/github/aastrandemma/springvalidatingform/SpringValidatingFormApplication.java
@@ -0,0 +1,13 @@
+package org.github.aastrandemma.springvalidatingform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValidatingFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringValidatingFormApplication.class, args);
+    }
+
+}
